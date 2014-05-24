b6a7791ad45d30196e5e7b051c6624fa2a0d89ab
Michael Whittaker <mjw297@cornell.edu>
2014-05-24 16:57:25 -0700

    Updated rust manifest

--- puppet/modules/rust/manifests/init.pp
+++ puppet/modules/rust/manifests/init.pp
@@ -3,13 +3,15 @@ include git
 class rust {
     exec { 'wget-rust': 
         command => "/usr/bin/wget http://static.rust-lang.org/dist/rust-nightly-i686-unknown-linux-gnu.tar.gz",
-        cwd     => "/tmp"
+        cwd     => "/tmp",
+        creates => "/tmp/rust-nightly-i686-unknown-linux-gnu.tar.gz";
     }
 
     exec { "tar-rust":
         command => "/bin/tar -xzvf rust-nightly-i686-unknown-linux-gnu.tar.gz",
         cwd     => "/tmp",
-        require => Exec["wget-rust"]
+        require => Exec["wget-rust"],
+        creates => "/tmp/rust-nightly-i686-unknown-linux-gnu";
     }
     
     exec { "install-rust":
