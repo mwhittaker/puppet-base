3dfdab64e410e6dc799c76318102a8f13b2db5db
Michael Whittaker <mjw297@cornell.edu>
2014-05-30 20:59:35 -0700

    Added mongorc.js

--- puppet/modules/mongo/manifests/init.pp
+++ puppet/modules/mongo/manifests/init.pp
@@ -2,4 +2,11 @@ class mongo {
     package { "mongodb":
         ensure => "installed";
     }
+
+    file { "/home/vagrant/.mongorc.js":
+        owner => 'vagrant',
+        group => 'vagrant',
+        mode  => '0644',
+        source => 'puppet:///modules/mongo/mongorc.js';
+    }
 }
